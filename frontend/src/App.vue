9149b816e77ae737c65fa610418f8506d8d525ee
Sheng Dong <Dongsheng600@outlook.com>
2024-11-09 22:13:09 +0000

    TodoView

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,31 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView, useRouter } from 'vue-router'
+import { ref } from 'vue'
+
+const activenum = ref(0)
+const pagename = ["todo", "schedule", "habit"]
+const router = useRouter()
+
+function nav(to) {
+  activenum.value = to
+  router.push({ name: pagename[to] })
+}
+
 </script>
 
 <template>
-  <div class="container-fluid vh-100 justify-content-between">
-    <div class="container-fluid h-100">
-      <RouterView />
-    </div>
-    <div class="container-fluid">
-      <nav class="nav nav-pills nav-justified">
-        <a class="nav-link" href="/todo">Todo</a>
-        <a class="nav-link" href="/schedule">Schedule</a>
-        <a class="nav-link" href="/habit">Habit</a>
-      </nav>
+  <div class="container-fluid vh-100 justify-content-between p-0">
+    <div class="d-flex flex-column h-100 p-0">
+      <div class="container-fluid flex-grow-1 px-0 overflow-y-hidden">
+        <RouterView />
+      </div>
+      <div class="container-fluid px-0 border-top">
+        <nav class="nav nav-pills nav-justified">
+          <a class="nav-link" :class="{ active: activenum == 0 }" @click="nav(0)">Todo</a>
+          <a class="nav-link" :class="{ active: activenum == 1 }" @click="nav(1)">Schedule</a>
+          <a class="nav-link" :class="{ active: activenum == 2 }" @click="nav(2)">Habit</a>
+        </nav>
+      </div>
     </div>
   </div>
 </template>
--- frontend/src/views/TodoView.vue
+++ frontend/src/views/TodoView.vue
@@ -1,6 +1,47 @@
 <script setup lang="js">
+
+import { ref } from 'vue';
+import TodoItem from "@/components/TodoItem.vue";
+
+const day = ref(10)
+
 </script>
 
 <template>
-    <h1> Hello Todo! </h1>
+    <div class="d-flex flex-column h-100">
+        <!-- <div class="row h-10">
+            <div class="col-3">
+                <button type="button" class="btn btn-light" @click="day -= 1">Yesterday</button>
+            </div>
+            <div class="col">
+                <h3 class="text-center mt-2">{{ day }}/11/2024</h3>
+            </div>
+            <div class="col-3">
+                <button type="button" class="btn btn-light" @click="day += 1">Tomorrow</button>
+            </div>
+        </div> -->
+        <div class="row flex-grow-1 h-100 overflow-y-auto">
+            <div class="container h-100">
+                <template v-for="item in [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]">
+                    <TodoItem />
+                </template>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-group mb-3 px-3">
+                <input class="form-control form-control-lg" type="text" placeholder="What do you want to do?">
+                <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom">Add</button>
+            </div>
+        </div>
+        <div class="offcanvas offcanvas-bottom px-0" tabindex="-1" id="offcanvasBottom"
+            aria-labelledby="offcanvasBottomLabel">
+            <div class="offcanvas-header px-1">
+                <input class="form-control form-control-lg" type="text" placeholder="What do you want to do?">
+            </div>
+            <div class="offcanvas-body small">
+                
+            </div>
+        </div>
+    </div>
 </template>

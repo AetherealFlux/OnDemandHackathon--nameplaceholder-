facbc39ce1262db6b21dac1c8904803f43cf06d6
EthanLi <ethan_aux@163.com>
2024-11-10 00:52:54 +0000

    Added components for habit item.

---
+++ frontend/src/components/HabitItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="js">
+defineProps({ name: String, description: String, plan: String })
+</script>
+<template>
+  <div class="row border-top border-bottom py-2 px-4">
+    <div class="row">
+      <span class="p-0">
+        <h3>Name: {{ name }}</h3>
+      </span>
+    </div>
+    <div class="row">Description: {{ description }}</div>
+  </div>
+</template>
